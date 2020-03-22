7d5d95e134dd6e51bd4d4646dc9f6531dfdf5008
Effy Yu <Effy.Yu@magna.com>
2020-03-22 15:25:38 +0800

    Nothing Change!

--- week1/week1/Form1.cs
+++ week1/week1/Form1.cs
@@ -19,7 +19,7 @@ namespace week1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Second Change.");
+            MessageBox.Show("Nothing Change!");
         }
     }
 }
